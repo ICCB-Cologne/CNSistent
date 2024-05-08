882ce9f3dd0ef11b04deac8aca7b972e5791ef1a
Adam Streck <adam.streck@gmail.com>
2024-05-08 13:13:44 +0200

    loading utility functions

--- cns/data_utils.py
+++ cns/data_utils.py
@@ -3,6 +3,8 @@ from cns.utils.files import load_cns, load_samples
 import pandas as pd
 from os.path import join as pjoin, abspath, dirname
 
+from cns.utils.selection import filter_samples, select_CNS_samples
+
 
 def get_root_path():
     return abspath(pjoin(dirname(__file__), ".."))
@@ -56,3 +58,34 @@ def load_data():
 def load_data_file(filename):
     sep = "\t" if filename.endswith(".tsv") else ","
     return pd.read_csv(pjoin(data_path, filename), sep=sep)
+
+
+def load_filter_samples():
+    samples = {
+        "PCAWG": load_samples_out("PCAWG_samples.tsv"),
+        "TRACERx": load_samples_out("TRACERx_samples.tsv"),
+        "TCGA": load_samples_out("TCGA_hg19_samples.tsv")
+    }
+    for k, v in samples.items():
+        print(k)
+        min_frac = 0.95 if k != "TRACERx" else 0.85
+        samples[k] = filter_samples(v, cover_min_frac=min_frac, whitelist=k=="PCAWG")
+    return samples
+
+
+def load_filter_bins(samples, bin_size):
+    cns = {
+        "PCAWG": load_cns_out(f"PCAWG_bin_{bin_size}.tsv"),
+        "TRACERx": load_cns_out(f"TRACERx_bin_{bin_size}.tsv"),
+        "TCGA": load_cns_out(f"TCGA_hg19_bin_{bin_size}.tsv")
+    }
+    for k, v in cns.items():
+        cns[k] = select_CNS_samples(v, samples[k])
+    return cns
+
+
+def get_cns_for_type(cns, samples, type):
+	query = f"type == '{type}' | TCGA_type == '{type}'" if "TCGA_type" in samples.columns else f"type == '{type}'"
+	ids = samples.query(query).index
+	select_cns = cns.set_index("sample_id").loc[ids].reset_index()
+	return select_cns
--- cns/utils/selection.py
+++ cns/utils/selection.py
@@ -48,3 +48,26 @@ def only_sex(cns):
 
 def drop_Y(cns):
     return cns.query("chrom != 'chrY'")
+
+
+def filter_samples(samples, ane_min_frac = 0.001, cover_min_frac = 0.95, whitelist = False):
+    cn_netural = samples.query(f"ane_major_cn_frac_aut < {ane_min_frac} & ane_major_cn_frac_aut < {ane_min_frac}").index
+    print(len(cn_netural), "samples are CN neutral")
+    filtered = samples.query("(index not in @cn_netural)")
+
+    # Find samples with low coverage (below 95% in autosomes)
+    low_coverage = samples.query(f"cover_frac_aut < {cover_min_frac}").index
+    print(len(low_coverage), "samples have low coverage")
+    filtered = samples.query("(index not in @low_coverage)")
+
+    # Filter out CN neutral and low coverage samples 
+    if whitelist:
+        blacklisted = samples.query("whitelist == False").index
+        print(len(blacklisted), "samples are blacklisted")        
+        filtered = samples.query("(index not in @blacklisted)")
+
+    return filtered.copy()
+
+
+def select_CNS_samples(cns, samples):
+    return cns.query("sample_id in @samples.index")
