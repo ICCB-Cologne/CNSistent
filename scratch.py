e78976c285e9d37439f9d73d6e18781641016592
Adam_Streck <adam.streck@gmail.com>
2024-12-10 17:17:17 +0100

    Producing medicc files.

--- scratch.py
+++ scratch.py
@@ -1,4 +1,17 @@
 # %%
 import cns.data_utils as cdu
+import cns
+import pandas as pd
 # %%
-samples_df, cns_df = cdu.main_load("raw")
+samples_df, cns_df = cdu.main_load("imp", "PCAWG", False)
+# %%
+cns_df
+
+# %%
+for sample_id in samples_df.index:
+    single = cns_df.query(f"sample_id == '{sample_id}'")
+    second = single.copy()
+    second["sample_id"] = "second_sample"
+    double = pd.concat([single, second])
+    cns.save_cns(double, f"./for_med/{sample_id}_cns.csv")
+# %%
